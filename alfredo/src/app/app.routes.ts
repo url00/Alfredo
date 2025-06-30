4359a484dcad2540927036bc7561be3437014366
Nathan Bergeron <nathanbrgrn@gmail.com>
2025-06-30 14:17:37 -0500

    Clean up.

--- alfredo/src/app/app.routes.ts
+++ alfredo/src/app/app.routes.ts
@@ -4,13 +4,8 @@ import { SetupCompleteGuard } from './core/auth/setup-complete-guard';
 import { StatusComponent } from './features/status/status';
 import { SetupInProgressGuard } from './core/auth/setup-in-progress-guard';
 import { SettingsComponent } from './features/settings/settings';
-import { AiTestComponent } from './ai-test';
 
 export const routes: Routes = [
-  {
-    path: 'ai-test',
-    component: AiTestComponent
-  },
   {
     path: 'setup',
     component: SetupWizardComponent,
