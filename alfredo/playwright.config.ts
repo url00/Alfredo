d10b4246e95ec3fd964b8570b79dd13f084b1c65
url00 <nathanbrgrn@gmail.com>
2025-07-09 07:59:36 -0500

    Changed playwright test configuration to better support VS Code's playwright extension.

--- alfredo/playwright.config.ts
+++ alfredo/playwright.config.ts
@@ -1,52 +1,50 @@
 import { defineConfig, devices } from '@playwright/test';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const projects = [
+  {
+    name: 'chromium',
+    use: { ...devices['Desktop Chrome'], baseURL: 'http://localhost:4200' },
+    testIgnore: /.*\.ai\.spec\.ts/,
+  },
+  {
+    name: 'firefox',
+    use: { ...devices['Desktop Firefox'], baseURL: 'http://localhost:4200' },
+    testIgnore: /.*\.ai\.spec\.ts/,
+  },
+  {
+    name: 'webkit',
+    use: { ...devices['Desktop Safari'], baseURL: 'http://localhost:4200' },
+    testIgnore: /.*\.ai\.spec\.ts/,
+  },
+  {
+    name: 'ai',
+    use: { ...devices['Desktop Chrome'], baseURL: 'http://localhost:4200/Alfredo/' },
+    testMatch: /.*\.ai\.spec\.ts/,
+  },
+];
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
   testDir: './playwright',
-  testIgnore: /.*\.ai\.spec\.ts/,
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: undefined,
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:4200',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-  ],
-
-  /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
     url: 'http://localhost:4200',
     reuseExistingServer: !process.env.CI,
   },
+  projects: process.env.AI_TESTS === 'true'
+    ? projects.filter(p => p.name === 'ai')
+    : projects.filter(p => p.name !== 'ai'),
 });
